74b74ba1f51bcc5ae85703d7c41120d8beceef1b
sameer <sayyagari>
2012-12-27 16:38:12 +0530

    added bind

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@
  */
 var app = {
     initialize: function() {
-       
+        this.bind();
     },
     bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
